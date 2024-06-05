a72b195655570ddceb78191852ae2c912fbd8e1d
Martin Racana <ing.racana@gmail.com>
2024-06-06 00:21:47 +0800

    fix IOS backdrop header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { SearchBar } from "./SearchBar";
 
 const Header = () => {
   return (
-    <header className="flex items-center px-4 py-3 shadow-sm dark:bg-gray-950 w-full backdrop-blur-md z-40 fixed ">
+    <header className="flex items-center px-4 py-3 shadow-sm dark:bg-gray-950 w-full backdrop-blur-md z-40 fixed backdropIOS">
       <Link
         href="/"
         className="flex items-center gap-2 pr-2"
